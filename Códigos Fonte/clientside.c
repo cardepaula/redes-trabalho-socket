d7f59dfb28cf0aab6fec6fb31cb1eaccd3df2586
Marcos Antonio Carneiro de Paula <cardepaula@gmail.com>
2018-09-22 14:28:41 -0300

    Acessar arquivos

--- Códigos Fonte/clientside.c
+++ Códigos Fonte/clientside.c
@@ -1,91 +1,100 @@
- /* Client side code */	
- 
- #include <stdio.h>	
- #include <unistd.h>
- #include <stdlib.h>
- #include <errno.h>	
- #include <string.h>	
- #include <sys/types.h>	
- #include <sys/socket.h>	
- #include <netinet/in.h>	
- #include <arpa/inet.h>	
- #include <netdb.h>	
- #include <time.h>
- #include <ctype.h>
+/* Client side code */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#include <ctype.h>
 
 /* Constantes */
 #define FALSE 0;
 #define TRUE 1;
 #define DIGITOSCODUSER 4;
 
-typedef struct mensagem {
+typedef struct mensagem
+{
 	short port;
 	time_t td;
-	char autorizacao; 
+	char autorizacao;
 	char nomeUsuario[50];
 	int codUsuario;
-} message;
+} Message;
 
 /* Protótipo das funções utilizadas */
 int leituraUsuario();
 int isAllDigit();
 
-/* Esta função reporta erro e finaliza do programa */	
-static void	
-bail(const char *on_what) {	
-if ( errno != 0 ) {	
-	fputs(strerror(errno),stderr);	
-	fputs("	 ",stderr);
-}	
-fputs(on_what,stderr);	
-fputc('\n',stderr);	
-exit(1);	
-}	
+/* Esta função reporta erro e finaliza do programa */
+static void
+bail(const char *on_what)
+{
+	if (errno != 0)
+	{
+		fputs(strerror(errno), stderr);
+		fputs("	 ", stderr);
+	}
+	fputs(on_what, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
 
-int main(int argc,char *argv[]) {	
+int main(int argc, char *argv[])
+{
 	int z;
 	char *srvr_addr = "127.0.0.1"; /* Usando endereço padrão */
-	char *srvr_port = "9099"; /* Usando porta como padrão */
-	struct sockaddr_in adr_srvr; /* AF_INET */
-	int len_inet; /* comprimento */
-	int s; /* Socket */
-	char dtbuf[128]; /* Informação de data e hora */
-	time_t td; /* Data e hora atual */
+	char *srvr_port = "9099";	  /* Usando porta como padrão */
+	struct sockaddr_in adr_srvr;   /* AF_INET */
+	int len_inet;				   /* comprimento */
+	int s;						   /* Socket */
+	char dtbuf[128];			   /* Informação de data e hora */
+	time_t td;					   /* Data e hora atual */
 	int n;
 	message msg;
 
 	/* Usuário só pode passar uma porta como argumento */
-	if ( argc != 2 ) {
+	if (argc != 2)
+	{
 		printf("Precisa passar uma porta que deseja entrar como argumento!\n");
 		exit(1);
 	}
 
 	/* Checa se o usuário passou na linha de comando digíto numérico válido para a identificação da porta */
-	if (!isAllDigit(argv[1])) {
+	if (!isAllDigit(argv[1]))
+	{
 		printf("Favor passar somente dígitos numéricos como argumento para a identificação da porta!\n");
 		exit(2);
 	}
 
-	/* Cria um socket do tipo TCP */		
-	s = socket(PF_INET, SOCK_STREAM, 0);		
-	if ( s == -1 ) {
+	/* Cria um socket do tipo TCP */
+	s = socket(PF_INET, SOCK_STREAM, 0);
+	if (s == -1)
+	{
 		bail("socket()");
 	}
-	
+
 	/* Preenche a estrutura do socket com a porta e endereço do servidor */
 	memset(&adr_srvr, 0, sizeof(adr_srvr));
 	adr_srvr.sin_family = AF_INET;
 	adr_srvr.sin_port = htons(atoi(srvr_port));
 	adr_srvr.sin_addr.s_addr = inet_addr(srvr_addr);
-	if ( adr_srvr.sin_addr.s_addr == INADDR_NONE ) {
+	if (adr_srvr.sin_addr.s_addr == INADDR_NONE)
+	{
 		bail("bad address.");
 	}
 
 	/* Conecta com o servidor */
 	len_inet = sizeof(adr_srvr);
 
-	z = connect(s, (struct sockaddr *) &adr_srvr, len_inet);
-	if ( z == -1 ) {
+	z = connect(s, (struct sockaddr *)&adr_srvr, len_inet);
+	if (z == -1)
+	{
 		bail("connect(2): Server isn't working.");
 	}
 
@@ -97,21 +106,22 @@ int main(int argc,char *argv[]) {
 	msg.codUsuario = codigoUsuario;
 
 	/* Escreve a mensagem para o socket do servidor */
-	z = write(s, (const void *) &msg, sizeof(message));
-	if ( z == -1 ) {
+	z = write(s, (const void *)&msg, sizeof(message));
+	if (z == -1)
+	{
 		bail("write(2): It's not possible to write on socket.");
 	}
-	
+
 	/* Pegando a informação de hora e data que o cliente faz a requisição ao servidor */
 	// time(&msg.td); // Pega hora atual
-	// n = (int)strftime(dtbuf,sizeof dtbuf,"%A %b %d %H:%M:%S %Y\n", localtime(&msg.td));	
+	// n = (int)strftime(dtbuf,sizeof dtbuf,"%A %b %d %H:%M:%S %Y\n", localtime(&msg.td));
 	// dtbuf[n] = 0;
-		
+
 	/* Lê as informações de data e hora do servidor */
 	// z = read(s, &msg, sizeof(message));
 	// if ( z == -1 ) {
 	// 	bail("read(2)");
-	// }	
+	// }
 	// printf("Usuário: %s\n", msg.nomeUsuario);
 	// printf("Porta: %d\n", msg.port);
 	// printf("Horário: %s\n", dtbuf);
@@ -122,41 +132,46 @@ int main(int argc,char *argv[]) {
 
 	// /*
 	// * Imprimi a data e a hora do servidor */
-	// printf("Nome: %s, data/hora: %s\n",msg.nome, dtbuf);	
+	// printf("Nome: %s, data/hora: %s\n",msg.nome, dtbuf);
 
-	/* Fecha o socket */	
-	close(s);	
+	/* Fecha o socket */
+	close(s);
 	putchar('\n');
 
-	return 0;	
+	return 0;
 }
 
 /* Faz a leitura da entrada de dados do usuário */
-int leituraUsuario() {
+int leituraUsuario()
+{
 	int codigoUsuario = 0;
 	char strCodigoUsuario[1000];
 	int qntDigitosCodUsuario = DIGITOSCODUSER;
 
 	printf("Digite o código de usuário: ");
-	scanf("%s", strCodigoUsuario);	
+	scanf("%s", strCodigoUsuario);
 
 	/* Usuário não pode digitar mais que 4 caracteres */
-	while ((strlen(strCodigoUsuario) != qntDigitosCodUsuario || !isAllDigit(strCodigoUsuario)) && strcmp(strCodigoUsuario, "-1") != 0) {
+	while ((strlen(strCodigoUsuario) != qntDigitosCodUsuario || !isAllDigit(strCodigoUsuario)) && strcmp(strCodigoUsuario, "-1") != 0)
+	{
 		printf("Favor digite somente %d dígitos numéricos.\n", qntDigitosCodUsuario);
 		printf("Digite o código de usuário: ");
 		scanf("%s", strCodigoUsuario);
 	}
 	codigoUsuario = atoi(strCodigoUsuario);
-	
-	return codigoUsuario;	
+
+	return codigoUsuario;
 }
 
 /* Checa se a string passada é de somente dígitos numéricos */
-int isAllDigit(char* strDigit) {
-	for (int i = 0; i < strlen(strDigit); i++) {
-		if (!isdigit(strDigit[i])) {
+int isAllDigit(char *strDigit)
+{
+	for (int i = 0; i < strlen(strDigit); i++)
+	{
+		if (!isdigit(strDigit[i]))
+		{
 			return FALSE;
-		} 
+		}
 	}
 	return TRUE;
 }
--- Códigos Fonte/serverside.c
+++ Códigos Fonte/serverside.c
@@ -1,4 +1,4 @@
-/* Server side code */	
+/* Server side code */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -12,119 +12,189 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-typedef struct mensagem {
+typedef struct mensagem
+{
 	short port;
 	time_t td;
-	char autorizacao; 
+	char autorizacao;
 	char nomeUsuario[50];
 	int codUsuario;
-} message;
+} Message;
 
 /* Esta função reporta erro e finaliza do programa: */
 static void
-bail(const char *on_what) {
-	if ( errno != 0 ) {
-		fputs(strerror(errno),stderr);
-		fputs(": ",stderr);
+bail(const char *on_what)
+{
+	if (errno != 0)
+	{
+		fputs(strerror(errno), stderr);
+		fputs(": ", stderr);
 	}
-	fputs(on_what,stderr);
-	fputc('\n',stderr);
+	fputs(on_what, stderr);
+	fputc('\n', stderr);
 	exit(1);
 }
 
-int main(int argc,char **argv) {
+int main(int argc, char **argv)
+{
 	int z;
 	char *srvr_addr = "127.0.0.1"; /* Endereço padrão */
-	char *srvr_port = "9099"; /* Porta padrão */
-	struct sockaddr_in adr_srvr;/* AF_INET */
-	struct sockaddr_in adr_clnt;/* AF_INET */
-	int len_inet; /* comprimento  */
-	int s; /* Socket do servidor */
-	int c; /* Socket par ao cliente */
-	int n; /* número de bytes lidos */
-	time_t td; /* Data e hora atual */
-	char dtbuf[128]; /* Informações de data e hora */
-	message msg;
+	char *srvr_port = "9099";	  /* Porta padrão */
+	struct sockaddr_in adr_srvr;   /* AF_INET */
+	struct sockaddr_in adr_clnt;   /* AF_INET */
+	int len_inet;				   /* comprimento  */
+	int s;						   /* Socket do servidor */
+	int c;						   /* Socket par ao cliente */
+	int n;						   /* número de bytes lidos */
+	time_t td;					   /* Data e hora atual */
+	char dtbuf[128];			   /* Informações de data e hora */
+	Message msg;
+	FILE *arqCredenciais;
+	FILE *arqRegistros;
+	char caminhoArqCredenciais[] = "/home/marcos/Insync/IFES/IFES 2018 2/Redes/Trabalho 1/redes-trabalho-socket/Arquivos/credenciais_de_usuario.txt";
+	char caminhoArqRegistros[] = "/home/marcos/Insync/IFES/IFES 2018 2/Redes/Trabalho 1/redes-trabalho-socket/Arquivos/arquivo_de_registro.txt";
+	char cTmp[1000];
 
- 	/* Cria um socket do tipo TCP */
-	s = socket(PF_INET, SOCK_STREAM, 0);
-	if ( s == -1 ) {
-		bail("socket()");
+	arqRegistros = fopen(caminhoArqRegistros, "a+");
+	if (arqRegistros == NULL)
+	{
+		printf("Error ao abrir o arquivo de registros\n");
+		exit(1);
 	}
 
-	/* Preenche a estrutura do socket com a porta e endereço do servidor */
-	memset(&adr_srvr, 0, sizeof(adr_srvr));
-	adr_srvr.sin_family = AF_INET;
-	adr_srvr.sin_port = htons(atoi(srvr_port));
-	if ( strcmp(srvr_addr,"*") != 0 ) {
-		/* Endereço normal */
-		adr_srvr.sin_addr.s_addr = inet_addr(srvr_addr);
-		if ( adr_srvr.sin_addr.s_addr == INADDR_NONE ) {
-			bail("bad address."); 
-		}
-	} else {
-		/* Qualquer endereço */
-		adr_srvr.sin_addr.s_addr = INADDR_ANY;
+	while (!feof(arqCredenciais))
+	{
+		fgets(cTmp, 1000, arqCredenciais);
+		printf("FGETS - %s", cTmp);
+		//fprintf(arqRegistros, "%s", cTmp);
 	}
 
-	/* Liga (bind) o socket com o endereço/porta */
-	len_inet = sizeof(adr_srvr);
-	z = bind(s, (struct sockaddr *)&adr_srvr, len_inet);
-	if ( z == -1 ) {
-		bail("bind(2)");
+	rewind(arqCredenciais);
+	while (!feof(arqCredenciais))
+	{
+		char str1[100], str2[100], str3[100];
+		fscanf(arqCredenciais, "%s,%s,%s", str1, str2, str3);
+		printf("\nFSCANF: %s\n", str1);
+		printf("FSCANF: %s\n", str2);
+		printf("FSCANF: %s\n", str3);
+
+		//fprintf(arqRegistros, "%s", cTmp);
 	}
 
-	/* Coloca o socket do servidor em estado de "escuta" com até 10 clientes simultâneos*/
-	z = listen(s, 10);
-	if ( z == -1 ) {
-		bail("listen(2)");
-	}
+	fclose(arqRegistros);
 
-	/* Inicia o loop do servido: */
-	while(1) {
+	// /* Cria um socket do tipo TCP */
+	// s = socket(PF_INET, SOCK_STREAM, 0);
+	// if (s == -1)
+	// {
+	// 	bail("socket()");
+	// }
 
-		/* Aguarda por uma solitação de conexão: */
-		len_inet = sizeof adr_clnt;
-		c = accept(s,(struct sockaddr *)&adr_clnt,&len_inet);
+	// /* Preenche a estrutura do socket com a porta e endereço do servidor */
+	// memset(&adr_srvr, 0, sizeof(adr_srvr));
+	// adr_srvr.sin_family = AF_INET;
+	// adr_srvr.sin_port = htons(atoi(srvr_port));
+	// if (strcmp(srvr_addr, "*") != 0)
+	// {
+	// 	/* Endereço normal */
+	// 	adr_srvr.sin_addr.s_addr = inet_addr(srvr_addr);
+	// 	if (adr_srvr.sin_addr.s_addr == INADDR_NONE)
+	// 	{
+	// 		bail("bad address.");
+	// 	}
+	// }
+	// else
+	// {
+	// 	/* Qualquer endereço */
+	// 	adr_srvr.sin_addr.s_addr = INADDR_ANY;
+	// }
 
-		if ( c == -1 ) {
-			bail("accept(2)");
-		}
-		
-		/* Gera a data e hora atual, onde o cliente faz requisições ao servidor: */
-		time(&msg.td);
-		n = (int) strftime(dtbuf,sizeof dtbuf, "%A %b %d %H:%M:%S %Y\n", localtime(&msg.td));
-		dtbuf[n] = 0;
+	// /* Liga (bind) o socket com o endereço/porta */
+	// len_inet = sizeof(adr_srvr);
+	// z = bind(s, (struct sockaddr *)&adr_srvr, len_inet);
+	// if (z == -1)
+	// {
+	// 	bail("bind(2)");
+	// }
 
-		z = read(c, &msg, sizeof(message));
-		if ( z == -1 ) {
-			bail("read(2): Its not possible to read the socket");
-		}
+	// /* Coloca o socket do servidor em estado de "escuta" com até 10 clientes simultâneos*/
+	// z = listen(s, 10);
+	// if (z == -1)
+	// {
+	// 	bail("listen(2)");
+	// }
 
-		//printf("Número da porta lida: %d\n", msg.port);
-		//printf("Código do cliente lido: %d\n", msg.codUsuario);
+	// /* Inicia o loop do servido: */
+	// while (1)
+	// {
 
-		/* Informações escritas para o usuário */
-		// strcpy(msg.nomeUsuario, "SEHLORO 157");
-		// msg.port = 157;
-		// msg.autorizacao = 'A';
-		// msg.td = time(&msg.td);
-		
-		/* Imprimi a data e a hora do servidor */
-	    //printf("Nome: %s, data/hora: %s\n",msg.nomeUsuario, dtbuf);
-		//printf("It's working!!!\n");
+	// 	/* Aguarda por uma solitação de conexão: */
+	// 	len_inet = sizeof adr_clnt;
+	// 	c = accept(s, (struct sockaddr *)&adr_clnt, &len_inet);
 
-		/* Escreve o resultado no socket do cliente */
-		//z = write(c,dtbuf,n);
-		// z = write(c,(const void *) &td,sizeof(time_t));
-		// z = write(c,(const void *) &msg,sizeof(struct mensagem));
-		// if ( z == -1 ) {
-		// 	bail("write(2)");
-		// }
-		
-		/* Fecha a conexão com o cliente corrente */
-		close(c);
-	}
+	// 	if (c == -1)
+	// 	{
+	// 		bail("accept(2)");
+	// 	}
+
+	// 	/* Gera a data e hora atual, onde o cliente faz requisições ao servidor: */
+	// 	time(&msg.td);
+	// 	n = (int)strftime(dtbuf, sizeof dtbuf, "%A %b %d %H:%M:%S %Y\n", localtime(&msg.td));
+	// 	dtbuf[n] = 0;
+
+	// 	z = read(c, &msg, sizeof(message));
+	// 	if (z == -1)
+	// 	{
+	// 		bail("read(2): Its not possible to read the socket");
+	// 	}
+
+	// 	//printf("Número da porta lida: %d\n", msg.port);
+	// 	//printf("Código do cliente lido: %d\n", msg.codUsuario);
+
+	// 	/* Informações escritas para o usuário */
+	// 	// strcpy(msg.nomeUsuario, "SEHLORO 157");
+	// 	// msg.port = 157;
+	// 	// msg.autorizacao = 'A';
+	// 	// msg.td = time(&msg.td);
+
+	// 	/* Imprimi a data e a hora do servidor */
+	// 	//printf("Nome: %s, data/hora: %s\n",msg.nomeUsuario, dtbuf);
+	// 	//printf("It's working!!!\n");
+
+	// 	/* Escreve o resultado no socket do cliente */
+	// 	//z = write(c,dtbuf,n);
+	// 	// z = write(c,(const void *) &td,sizeof(time_t));
+	// 	// z = write(c,(const void *) &msg,sizeof(struct mensagem));
+	// 	// if ( z == -1 ) {
+	// 	// 	bail("write(2)");
+	// 	// }
+
+	// 	/* Fecha a conexão com o cliente corrente */
+	// 	close(c);
+	// }
 
 	return 0;
+}
+
+int verificaCredenciais(char *caminhoArqCredenciais, Message *msg)
+{
+
+	char arqCredenciais[] = fopen(caminhoArqCredenciais, "r");
+	if (arqCredenciais == NULL)
+	{
+		printf("Error ao abrir o arquivo de Credenciais\n");
+		exit(1);
+	}
+
+	while (!feof(arqCredenciais))
+	{
+		fgets(cTmp, 1000, arqCredenciais);
+		printf("FGETS - %s", cTmp);
+	}
+
+	fclose(arqCredenciais);
+}
+
+void atualizaRegistro(char *arqRegistros, char *registro)
+{
 }
